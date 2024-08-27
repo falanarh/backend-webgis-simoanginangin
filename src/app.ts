625ace662c0cf5ba1e96ecb22952c8c83cabe20e
222112038 <falanarofako29@gmail.com>
2024-08-27 15:00:58 +0700

    update json limit

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 // app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
