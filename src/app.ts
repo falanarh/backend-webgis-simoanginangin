0450809146d2649bdc38ea4635e1b776a8af22f6
222112038 <falanarofako29@gmail.com>
2024-08-30 08:52:50 +0700

    add cors

--- src/app.ts
+++ src/app.ts
@@ -23,14 +23,20 @@ app.use(morgan("dev"));
 //   })
 // );
 
-app.use(cors({
-  origin: ['https://desa-cantik-sda.vercel.app', 'http://localhost:5173'], // Ganti dengan domain Anda
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Metode yang diizinkan
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: [
+      "https://desa-cantik-sda.vercel.app",
+      "http://localhost:5173",
+      "http://desacantik.pahlawan140.com",
+    ], // Ganti dengan domain Anda
+    methods: ["GET", "POST", "PUT", "DELETE"], // Metode yang diizinkan
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // app.use(express.json());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
 // app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
